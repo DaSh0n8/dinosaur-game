c8ef718288a5090f41e83c621163ed368810452c
znga0001 <znga0001@student.monash.edu>
2021-05-03 22:14:44 +0800

    Add some attributes and an enum called Gender in Dinosaur class, then modify Stegosaur class to extend Dinosaur class

--- src/game/Dinosaur.java
+++ src/game/Dinosaur.java
@@ -8,13 +8,22 @@ import edu.monash.fit2099.engine.Actor;
 public abstract class Dinosaur extends Actor {
 
     private Behaviour behaviour;
+    private int MAX_FOOD_LEVEL;
+    private int current_food_level;
+
+    /**
+     * Enum represents gender of the dinosaur.
+     */
+    private enum Gender {
+        MALE, FEMALE
+    }
 
     /**
      * Constructor.
      *
-     * @param name the name of this Dinosaur
+     * @param name        the name of this Dinosaur
      * @param displayChar the character that will represent this type of Dinosaur
-     * @param hitPoints the Actor's starting hit points
+     * @param hitPoints   the Actor's starting hit points
      */
     public Dinosaur(String name, char displayChar, int hitPoints) {
         super(name, displayChar, hitPoints);
