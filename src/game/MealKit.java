4a68d54d8fcb9c750be0747369a11b9596f50263
Brandon <blim0014@student.monash.edu>
2021-05-07 23:50:54 +1000

    Eggs and MealKit creation

---
+++ src/game/MealKit.java
@@ -0,0 +1,60 @@
+package game;
+
+import edu.monash.fit2099.engine.Item;
+
+public abstract class MealKit extends Item {
+    public MealKit(String name, char displayChar, boolean portable) {
+        super(name, displayChar, portable);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/game/VegetarianMealKit.java
@@ -0,0 +1,7 @@
+package game;
+
+public class VegetarianMealKit extends MealKit{
+    public VegetarianMealKit() {
+        super("Vegetarian Meal Kit", 'V', true);
+    }
+}
